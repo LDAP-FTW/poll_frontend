6375098a25f81a9c779104e309ab583606d3ad26
Nico Freundt <nico.freundt@protonmail.com>
2022-05-31 17:21:04 +0200

    removed unneccessary src

--- src/components/ResponsiveAppBar.jsx
+++ src/components/ResponsiveAppBar.jsx
@@ -134,7 +134,7 @@ const ResponsiveAppBar = () => {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt={auth().user.username} src="/static/images/avatar/2.jpg" />
+                <Avatar alt={auth().user.username}/>
               </IconButton>
             </Tooltip>
             <Menu
