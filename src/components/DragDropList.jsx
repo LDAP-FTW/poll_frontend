682984f1a7e8913d0a0e50b46bfb1fa0a37895d5
Nico Freundt <nico.freundt@protonmail.com>
2022-05-31 13:34:59 +0200

    outsourced list into new component

---
+++ src/components/DragDropList.jsx
@@ -0,0 +1,71 @@
+import { Box, Button, Typography } from '@mui/material'
+import React from 'react'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
+
+
+const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+}
+
+const DragDropList = ({ list, setList }) => {
+
+    const deleteQuestion = (questionId) => {
+        let questionsCopy = _.cloneDeep(list);
+        questionsCopy.splice(questionId, 1);
+        setList(questionsCopy);
+    }
+
+    const onDragEnd = (result) => {
+        if (!result.destination) {
+            return;
+        }
+
+        if (result.destination.index === result.source.index) {
+            return;
+        }
+
+        const newOrderedQuestions = reorder(
+            list,
+            result.source.index,
+            result.destination.index
+        );
+
+        setList(newOrderedQuestions);
+    }
+
+    return (
+        <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId='list'>
+                {(provided) =>
+                    <Box
+                        ref={provided.innerRef}
+                        {...provided.droppableProps}
+                        sx={{ display: "flex", flexDirection: "column", my: 2 }}>
+                        {list.map((text, id) =>
+                            <Draggable key={id} draggableId={`${id}`} index={id}>
+                                {(provided) =>
+                                    <Box
+                                        sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between", gap: "15px", alignItems: "center", background: "black", mb: 2 }}
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}
+                                    >
+                                        <Typography sx={{ overflow: "hidden" }} variant='p'>{text}</Typography>
+                                        <Button variant='outlined' onClick={() => deleteQuestion(id)}>Delete</Button>
+                                    </Box>
+                                }
+                            </Draggable>
+                        )}
+                        {provided.placeholder}
+                    </Box>
+                }
+            </Droppable>
+        </DragDropContext>
+    )
+}
+
+export default DragDropList
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Box, Button, TextField, Typography } from '@mui/material';
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import DragDropList from '../components/DragDropList';
 
 const Create = () => {
 
@@ -8,41 +9,9 @@ const Create = () => {
     const [questions, setQuestions] = useState([]);
     const [question, setQuestion] = useState("");
 
-    const deleteQuestion = (questionId) => {
-        let questionsCopy = _.cloneDeep(questions);
-        questionsCopy.splice(questionId, 1);
-        setQuestions(questionsCopy);
-    }
-
     const addQuestion = () => {
         setQuestions([...questions].concat(question));
         setQuestion("");
-    }
-
-    const onDragEnd = (result) => {
-        if (!result.destination) {
-            return;
-        }
-
-        if (result.destination.index === result.source.index) {
-            return;
-        }
-
-        const newOrderedQuestions = reorder(
-            questions,
-            result.source.index,
-            result.destination.index
-        );
-
-        setQuestions(newOrderedQuestions);
-    }
-
-    const reorder = (list, startIndex, endIndex) => {
-        const result = Array.from(list);
-        const [removed] = result.splice(startIndex, 1);
-        result.splice(endIndex, 0, removed);
-
-        return result;
     }
 
     const uploadPoll = async () => {
@@ -64,33 +33,7 @@ const Create = () => {
             <Typography sx={{ mb: 1 }} variant="h5">Titel</Typography>
             <TextField value={title} onChange={(event) => setTitle(event.target.value)} sx={{ width: "100%", mb: 2 }} label="Titel" variant='outlined' />
             <Typography sx={{ mb: 1 }} variant="h5">Fragen</Typography>
-            <DragDropContext onDragEnd={onDragEnd}>
-                <Droppable droppableId='list'>
-                    {(provided) =>
-                        <Box 
-                        ref={provided.innerRef} 
-                        {...provided.droppableProps}
-                        sx={{ display: "flex", flexDirection: "column", gap: "15px", my: 2 }}>
-                            {questions.map((text, id) =>
-                                <Draggable key={id} draggableId={`${id}`} index={id}>
-                                    {(provided) =>
-                                        <Box
-                                            sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between", gap: "15px", alignItems: "center", background: "black" }}
-                                            ref={provided.innerRef}
-                                            {...provided.draggableProps}
-                                            {...provided.dragHandleProps}
-                                        >
-                                            <Typography sx={{ overflow: "hidden" }} variant='p'>{text}</Typography>
-                                            <Button variant='outlined' onClick={() => deleteQuestion(id)}>Delete</Button>
-                                        </Box>
-                                    }
-                                </Draggable>
-                            )}
-                            {provided.placeholder}
-                        </Box>
-                    }
-                </Droppable>
-            </DragDropContext>
+            <DragDropList list={questions} setList={setQuestions} />
             <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "baseline", gap: "15px" }}>
                 <TextField sx={{ width: "90%", mb: 2 }} value={question} onChange={(event) => setQuestion(event.target.value)} label="Neue Frage" variant='outlined' />
                 <Button onClick={addQuestion} variant="contained">Add</Button>
